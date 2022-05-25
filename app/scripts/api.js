8d402a54fa56d84031da4008b2bf9129814d2e59
Tiphaine du Réau <tiphaine.dureau@gmail.com>
2022-05-25 16:33:14 +0200

    Added GET and PUT form in CC6 form

--- app/scripts/api.js
+++ app/scripts/api.js
@@ -1,46 +1,4 @@
 /**
- * Utilisation de l'Api Fetch avec la méthode POST pour ajouter les données saisies dans un nouveau document de la bdd
- * @param formTwo
- */
-function postForm(formTwo) {
-  fetch(`${window.location.origin}/simulator`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-    },
-    body: JSON.stringify(formTwo)
-  }).then((response) => {
-    return response.json()
-      .then(function (data) {
-        // TODO hide spinner
-        // TODO show success toast
-      });
-  })
-}
-
-/**
- * Utilisation de l'api Fetch avec la méthode PUT pour que les données saisies modifient les données de la bdd
- * @param formData
- */
-function putForm(formData) {
-  fetch(`${window.location.origin}/simulator/${currentDocId}`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-    },
-    body: JSON.stringify(formData)
-  }).then((response) => {
-    return response.json()
-      .then(function (data) {
-        // TODO hide spinner
-        // TODO show success toast
-      });
-  })
-}
-
-/**
  * Appelle l'API GET cc6Form avec l'année courante en paramètre
  * @returns {Promise<any>} Les données du formulaire CC6 de l'année courante
  */
@@ -51,20 +9,17 @@ async function getCC6Form() {
   })
 }
 
-/**
- * Récupération de l'id courant
- * Récupération des données de la bdd pour les afficher
- */
-function initFetching() {
-  fetch(`${window.location.origin}/ping`, { method: 'GET' }).then((response) => {
-    return response.json().then(function (data) {
-      document.getElementById('testDatabase').innerText = data.size;
-    });
-  })
-
-  fetch(`${window.location.origin}/simulator`, { method: 'GET' }).then((response) => {
-    return response.json().then(function (data) {
-      currentDocId = data._id;
-    });
+async function putCC6Form(formData) {
+  const id = formData._id;
+  delete formData._id;
+  return fetch(`${window.location.origin}/cc6Form/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  }).then((response) => {
+    return response.json().then((data) => data);
   })
 }
--- server.js
+++ server.js
@@ -38,56 +38,24 @@ mongoClient.connect(() => {
   console.info("logged to database");
 });
 
-
-/**
- * Pour recevoir / requêter une donnée de la base de données
- * https://www.mongodb.com/docs/v4.2/reference/method/db.collection.find/
- * Indique le nom de la collection et le document associé de mongodb
- * Convertis en array le résultat
- */
-server.get('/ping', function (req, res) {
-  console.info("called ping endpoint");
-  const collection = mongoClient.db('FenixTechDatabase').collection('test')
-  collection.find({}).toArray(function (err, results) {
+server.get('/cc6Form', function (req, res) {
+  console.info("called cc6 form endpoint");
+  const collection = mongoClient.db('FenixTechDatabase').collection('FormCC6')
+  // TODO if document is not found, create a new empty document for this year
+  collection.find({year: req.query['year']}).toArray(function (err, results) {
     res.send(results[0]);
   })
 })
 
-server.get('/simulator', function (req, res) {
-  console.info("called ping endpoint");
-  const collection = mongoClient.db('FenixTechDatabase').collection('test')
-  collection.find({}).toArray(function (err, results) {
-    res.send(results[1]);
-  })
-})
-
 /**
- * Pour modifier des données de la base de données
+ * Pour modifier des données CC6 de la base de données
  */
-
-server.put('/simulator/:id', function (req, res) {
+server.put('/cc6Form/:id', function (req, res) {
   const reqId = req.params.id;
-  console.info("called simulator endpoint with id : " + reqId);
-  const collection = mongoClient.db('FenixTechDatabase').collection('test');
+  console.info("called cc6 form endpoint with id : " + reqId);
+  const collection = mongoClient.db('FenixTechDatabase').collection('FormCC6');
   collection.updateOne({ _id: ObjectId(reqId) }, { $set: req.body }).then(() => {
     collection.find({ _id: ObjectId(reqId) }).toArray(function (err, results) {
-      res.statusCode = 200;
-      res.send(results[0]);
-    })
-  }).catch(() => {
-    res.statusCode = 500;
-    res.send();
-  })
-})
-
-/**
- * Pour ajouter des données dans la base de donnée
- */
-server.post('/simulator', function (req, res) {
-  console.info("called simulator endpoint and create new simulation");
-  const collection = mongoClient.db('FenixTechDatabase').collection('test');
-  collection.insertOne({ ...req.body }).then((doc) => {
-    collection.find({ _id: ObjectId(doc.insertedId) }).toArray(function (err, results) {
       res.statusCode = 200;
       res.send(results[0]);
     })
