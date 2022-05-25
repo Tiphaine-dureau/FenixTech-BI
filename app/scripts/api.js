e0c60e0338c51d434924f124dd93197a51e1ec91
Tiphaine du Réau <tiphaine.dureau@gmail.com>
2022-05-25 18:37:09 +0200

    Ft-add-Fetch-POST-FormV

--- app/scripts/api.js
+++ app/scripts/api.js
@@ -9,11 +9,34 @@ async function getCC6Form() {
   })
 }
 
+/**
+ * Modifie les données de la base de données avec les données remplies
+ * @param formData
+ * @returns {Promise<any>}
+ */
 async function putCC6Form(formData) {
   const id = formData._id;
   delete formData._id;
   return fetch(`${window.location.origin}/cc6Form/${id}`, {
     method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  }).then((response) => {
+    return response.json().then((data) => data);
+  })
+}
+
+/**
+ * Créer dans la base de donnée un nouveau document avec les données remplies
+ * @param formData
+ * @returns {Promise<any>}
+ */
+async function postVForm(formData) {
+  return fetch(`${window.location.origin}/vForm`, {
+    method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       'Accept': 'application/json'
